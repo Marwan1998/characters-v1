4cc99f61d6364699e9ec46dbbdfbf3af05362b67
Marwan Algadi <marwan.algadi1998@gmail.com>
2021-06-26 01:33:55 +0200

    rendering data from the Database

--- app.js
+++ app.js
@@ -5,98 +5,93 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("/public"));
 
-
-
-app.get('/', (req, res) => {
-  //TODO: here i will add find all the doc in mongodb and send it to the view
-
-
-  
-  res.render('index', {characters});
+mongoose.connect("mongodb://localhost:27017/CharactersDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+const characterSchema = mongoose.Schema({
+  id: Number,
+  img1: String,
+  img2: String,
+  img3: String,
+  animNam: String,
+  charNam: String,
+});
 
+const Character = mongoose.model("Character", characterSchema);
+
+app.get("/", async (req, res) => {
+
+  //TODO: Fix The Promise.
+  const characters = await getData();
+
+  res.render("index", {characters});
+});
 
 app.get("/insert.ejs", function (req, res) {
-  res.render('insert')
+  res.render("insert");
 });
 
-app.post("/insert.ejs", function(req, res){
+app.post("/insert.ejs", function (req, res) {
   //TODO: here i will only save the data in the mongodb db then redirect the home page
 
-  const char = {
-    id: "giveID",
+  //TODO: find the last id and use it to the new one.
+
+  const char = new Character({
+    id: 4, // Needs an id here
     img1: req.body.img1,
     img2: req.body.img2,
     img3: req.body.img3,
     animNam: req.body.animNam,
     charNam: req.body.charNam,
-  }
+  });
 
-  res.redirect('/');
+  char.save(function (err) {
+    if (err) {
+      console.log(err);
+    } //TODO: Handel the error, re enter the values if error
+  });
+
+  res.redirect("/");
 });
 
-
-
-
-
-
-
-
-
-
-
 // to get all the css styles that required in html page
-app.get('/css/:css', function (req, res) {
-    const css = req.params.css;
-    res.sendFile(__dirname + `/public/css/${css}`);
+app.get("/css/:css", function (req, res) {
+  const css = req.params.css;
+  res.sendFile(__dirname + `/public/css/${css}`);
 });
 
 // to get all js file that required in html page
-app.get('/src/:src', function (req, res) {
-    const src = req.params.src;
-    res.sendFile(__dirname + `/public/src/${src}`);
+app.get("/src/:src", function (req, res) {
+  const src = req.params.src;
+  res.sendFile(__dirname + `/public/src/${src}`);
 });
 
 // to get all the images that required in html page
-app.get('/images/:imgNum', function(req, res){
-    const imgNum = req.params.imgNum;
-    res.sendFile(__dirname + `/public/images/${imgNum}`);
+app.get("/images/:imgNum", function (req, res) {
+  const imgNum = req.params.imgNum;
+  res.sendFile(__dirname + `/public/images/${imgNum}`);
 });
-
-
 
 app.listen(3000, () => console.log("Server running"));
 
 
-const characters = [
-  {
-      id: 1,
-      img1: './images/1.jpg',
-      img2: './images/2.jpg',
-      img3: './images/3.jpg',
-      charNam: "Luffy",
-      animNam: "Onepiece",
-  },
-  {
-      id: 2,
-      img1: './images/4.jpeg',
-      img2: './images/3.jpg',
-      img3: './images/2.jpg',
-      charNam: "Gon",
-      animNam: "hinter x hinter",
-  },
-  {
-      id: 3,
-      img1: './images/1.jpg',
-      img2: './images/2.jpg',
-      img3: './images/3.jpg',
-      charNam: "Nimi",
-      animNam: "OnePice",
-  },
-];
+
+
+async function getData() {
+  let characters;
+  await Character.find(function (err, character) {
+    if (!err) {
+      characters = character;
+    } else {
+      characters = []; // To avoid the undefind value.
+    }
+  });
+  return characters;
+}
