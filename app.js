62654f01a045c443b2a0b3a058f3c8894b43b634
marwan Algadi <marwanalgadi@work.com>
2021-06-29 01:01:52 +0200

    added error protection in saving data, done some refactoring

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+/* Characters Web App v1.0 Created By Marwan Algadi, MG45.
+
+TODO1: one todo after release is to add the Alert in error in saving data
+
+run 'npm i' to install all required packages 
+
+in next version 'v1.1' there will be:
+- fix the footer layout
+- delete Item functionality
+- add Upload photos insted of writing the URL
+- add Search functionality
+
+*/
+
+
 const express = require("express");
 const mongoose = require("mongoose");
-// let ejs = require('ejs');
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -26,7 +40,7 @@ const characterSchema = mongoose.Schema({
 
 const Character = mongoose.model("Character", characterSchema);
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
 
   getData()
   .then(characters => res.render("index", {characters}))
@@ -58,12 +72,14 @@ app.post("/insert.ejs", async function (req, res) {
   });
 
   char.save(function (err) {
-    if (err) {
-      console.log(err);
-    } //TODO: Handel the error, re enter the values if error
+    if (!err) {
+      res.redirect("/");
+    }else{
+      console.log("error saving the data: " + err);
+      // alert("please enter correct values"); // TODO 1:find some alert here
+      res.render("insert.ejs");
+    }
   });
-
-  res.redirect("/");
 });
 
 // to get all the css styles that required in html page
