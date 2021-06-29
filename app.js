713bd09e04ac8293b0a6024992103c0ce2cced7d
Marwan Algadi <marwan.algadi1998@gmail.com>
2021-06-29 20:08:42 +0200

    added error warning in saving data to the DB, some refactoring

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 /* Characters Web App v1.0 Created By Marwan Algadi, MG45.
 
-TODO1: one todo after release is to add the Alert in error in saving data
-
 run 'npm i' to install all required packages 
 
 in next version 'v1.1' there will be:
@@ -11,7 +9,6 @@ in next version 'v1.1' there will be:
 - add Search functionality
 
 */
-
 
 const express = require("express");
 const mongoose = require("mongoose");
@@ -41,29 +38,35 @@ const characterSchema = mongoose.Schema({
 const Character = mongoose.model("Character", characterSchema);
 
 app.get("/", (req, res) => {
-
   getData()
-  .then(characters => res.render("index", {characters}))
-  .catch((err) => {
-    const characters = [{id:0, img1:"", img2:"", img3:"", animNam:"Error", charNam:"Error"}];
-    console.log(err);
-    res.render("index", {characters}) // To avoid the undefind value.
-  });
-
+    .then((characters) => res.render("index", {characters}))
+    .catch((err) => {
+      const characters = [
+        {
+          id: 0,
+          img1: "",
+          img2: "",
+          img3: "",
+          animNam: "Error",
+          charNam: "Error",
+        },
+      ];
+      console.log(err);
+      res.render("index", {characters}); // Empty values to avoid the undefined value.
+    });
 });
 
-app.get("/insert.ejs", function (req, res) {
-  res.render("insert");
-});
+app.get("/insert.ejs", (req, res) => res.render("insert", {isError: false}));
 
 app.post("/insert.ejs", async function (req, res) {
-
-  let id ; 
-  await getLastID().then(value => id = value).catch(err => id=999); //get the last object's id from the DB
-  id+=1; // increase the id by one.
+  let id;
+  await getLastID()
+    .then((value) => (id = value))
+    .catch((err) => (id = 999)); //get the last object's id from the DB
+  id += 1; // increase the id by one.
 
   const char = new Character({
-    id: id, 
+    id: id,
     img1: req.body.img1,
     img2: req.body.img2,
     img3: req.body.img3,
@@ -74,35 +77,24 @@ app.post("/insert.ejs", async function (req, res) {
   char.save(function (err) {
     if (!err) {
       res.redirect("/");
-    }else{
+    } else {
       console.log("error saving the data: " + err);
-      // alert("please enter correct values"); // TODO 1:find some alert here
-      res.render("insert.ejs");
+      res.render("insert", {isError: true}); // send an error flag to html
     }
   });
 });
 
 // to get all the css styles that required in html page
-app.get("/css/:css", function (req, res) {
-  const css = req.params.css;
-  res.sendFile(__dirname + `/public/css/${css}`);
-});
-
-// to get all js file that required in html page
-app.get("/src/:src", function (req, res) {
-  const src = req.params.src;
-  res.sendFile(__dirname + `/public/src/${src}`);
-});
+app.get("/css/:css", (req, res) =>
+  res.sendFile(__dirname + `/public/css/${req.params.css}`)
+);
 
 // to get all the images that required in html page
-app.get("/images/:imgNum", function (req, res) {
-  const imgNum = req.params.imgNum;
-  res.sendFile(__dirname + `/public/images/${imgNum}`);
-});
+app.get("/images/:imgNum", (req, res) =>
+  res.sendFile(__dirname + `/public/images/${req.params.imgNum}`)
+);
 
 app.listen(3000, () => console.log("Server running"));
-
-
 
 async function getData() {
   let characters;
@@ -116,14 +108,14 @@ async function getData() {
   return characters;
 }
 
-async function getLastID(){
-  let id ;
+async function getLastID() {
+  let id;
   await Character.find(function (err, character) {
     if (!err) {
-      id = character[character.length-1].id;
-    }else{
+      id = character[character.length - 1].id;
+    } else {
       id = 999; // to avoid the undefined id value
     }
-  }).sort({id:1});
+  }).sort({id: 1});
   return id;
 }
